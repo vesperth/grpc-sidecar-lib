aa9cd14ab301d0a7fca0faf95d9bebf7d22b4713
PolarRegionLee <1152411160@qq.com>
2026-02-17 15:14:16 +0800

    first commit

---
+++ pkg/handler.go
@@ -0,0 +1,127 @@
+package pkg
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"io"
+	"net/http"
+	"strings"
+
+	"github.com/gin-gonic/gin"
+	"github.com/jhump/protoreflect/desc"
+	"github.com/jhump/protoreflect/dynamic"
+	"github.com/jhump/protoreflect/dynamic/grpcdynamic"
+	"github.com/pkg/errors"
+	"github.com/vesperth/grpc-sidecar-lib/utils"
+	spb "google.golang.org/genproto/googleapis/rpc/status"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/metadata"
+	"google.golang.org/grpc/status"
+)
+
+type BeforeCallOptionFuncParam struct {
+	ServiceName string
+	MethodDesc  *desc.MethodDescriptor
+	HttpContext *gin.Context
+	RequestMsg  *dynamic.Message
+}
+
+type BeforeCallOptionFunc func(param *BeforeCallOptionFuncParam) (grpc.CallOption, error)
+
+type AfterCallOptionFuncParam struct {
+	ServiceName string
+	MethodDesc  *desc.MethodDescriptor
+	GinContext  *gin.Context
+	ResponseMsg *dynamic.Message
+}
+
+type AfterCallOptionFunc func(param *AfterCallOptionFuncParam) (grpc.CallOption, error)
+
+type ServiceHandlerOptions struct {
+	BeforeCallOptionFuncs []BeforeCallOptionFunc
+	AfterCallOptionFuncs  []AfterCallOptionFunc
+}
+
+func newServiceHandler(serviceClientConn *grpc.ClientConn, method *desc.MethodDescriptor,
+	allowedRequestHeaders []string) gin.HandlerFunc {
+	return func(c *gin.Context) {
+		r, w := c.Request, c.Writer
+		requestMD := metadata.New(map[string]string{})
+		var originHeader = c.Request.Header.Clone()
+		c.Request.Header = make(http.Header)
+		for _, headerKey := range allowedRequestHeaders {
+			var values = originHeader.Values(headerKey)
+			for _, value := range values {
+				requestMD.Append(headerKey, value)
+			}
+		}
+		//requestMD.Append("Connection", "keep-alive")
+		//httpContext, cancelFunc := context.WithTimeout(c.Request.Context(), 1*time.Hour)
+		//defer cancelFunc()
+		var httpContext = context.Background()
+		var rpcRequestCtx = metadata.NewOutgoingContext(httpContext, requestMD)
+
+		// 解析HTTP请求体到动态消息
+		dynamicMsg := dynamic.NewMessage(method.GetInputType())
+		if r.Body == http.NoBody {
+			var emptyJsonReader = strings.NewReader("{}")
+			r.Body = io.NopCloser(emptyJsonReader)
+		}
+		if err := json.NewDecoder(r.Body).Decode(dynamicMsg); err != nil {
+			http.Error(w, fmt.Sprintf("Failed to decode request body: %v", err), http.StatusBadRequest)
+			return
+		}
+
+		// 使用grpcdynamic包执行gRPC请求
+		stub := grpcdynamic.NewStub(serviceClientConn)
+		response, err := stub.InvokeRpc(rpcRequestCtx, method, dynamicMsg)
+		if err != nil {
+			handleInvokeError(c, err)
+			return
+		}
+
+		// 将响应转换回JSON并返回
+		var responseMsg = dynamic.NewMessage(method.GetOutputType())
+		if err := responseMsg.MergeFrom(response); err != nil {
+			var internalErr = errors.Wrap(err, "merge response")
+			http.Error(w, fmt.Sprintf("Failed to merge response: %v", internalErr),
+				http.StatusInternalServerError)
+			return
+		}
+
+		respJSON, err := responseMsg.MarshalJSON()
+		if err != nil {
+			http.Error(w, fmt.Sprintf("Failed to marshal response: %v", err),
+				http.StatusInternalServerError)
+			return
+		}
+
+		w.Header().Set("Content-Type", "application/json")
+		if _, err := w.Write(respJSON); err != nil {
+			http.Error(w, fmt.Sprintf("Failed to write response: %v", err),
+				http.StatusInternalServerError)
+			return
+		}
+	}
+}
+
+func handleInvokeError(c *gin.Context, err error) {
+	statusObj, ok := status.FromError(err)
+	if !ok {
+		c.JSON(http.StatusInternalServerError, gin.H{
+			"code": "Unsorted",
+			"msg":  err.Error(),
+		})
+		return
+	}
+	type StatusMirror struct {
+		s *spb.Status
+	}
+	var statusMirror = utils.UnsafeCast[*status.Status, *StatusMirror](statusObj)
+	c.JSON(http.StatusInternalServerError, gin.H{
+		"code":    statusObj.Code().String(),
+		"msg":     statusObj.Message(),
+		"details": statusMirror.s.Details,
+	})
+}
---
+++ pkg/proxy.go
@@ -0,0 +1,255 @@
+package pkg
+
+import (
+	"context"
+	"fmt"
+	"net/http"
+	"net/url"
+	"reflect"
+	"strconv"
+	"strings"
+	"time"
+
+	"github.com/gin-gonic/gin"
+	"github.com/golang/protobuf/proto"
+	openAPIV3Proto "github.com/google/gnostic/openapiv3"
+	"github.com/jhump/protoreflect/desc"
+	"github.com/jhump/protoreflect/dynamic"
+	"github.com/jhump/protoreflect/grpcreflect"
+	"github.com/pkg/errors"
+	"github.com/swaggest/openapi-go"
+	"github.com/swaggest/openapi-go/openapi3"
+	"github.com/vesperth/grpc-sidecar-lib/config"
+	"github.com/vesperth/grpc-sidecar-lib/pkg/http_rule"
+	"github.com/vesperth/grpc-sidecar-lib/static"
+	"google.golang.org/genproto/googleapis/api/annotations"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/keepalive"
+	"google.golang.org/protobuf/types/descriptorpb"
+)
+
+func ApplyHttpEngineFromConfig(engine gin.IRouter, sidecarConfig *config.SidecarConfig) {
+	var kacp = keepalive.ClientParameters{
+		Time:                30 * time.Second, // send pings every 10 seconds if there is no activity
+		Timeout:             10 * time.Second, // wait 1 second for ping back
+		PermitWithoutStream: true,             // send pings even without active streams
+	}
+	// 连接到gRPC服务
+	var defaultCallOptions = []grpc.CallOption{
+		grpc.MaxCallRecvMsgSize(1024 * 1024 * 1024),
+	}
+	conn, err := grpc.Dial(sidecarConfig.BackendAddress,
+		grpc.WithInsecure(),
+		grpc.WithKeepaliveParams(kacp),
+		grpc.WithDefaultCallOptions(defaultCallOptions...),
+		grpc.WithBlock())
+	if err != nil {
+		panic(errors.Wrap(err, "dial grpc"))
+	}
+
+	// 使用gRPC reflection创建一个reflection client
+	refClient := grpcreflect.NewClientAuto(context.Background(), conn)
+	serviceNames, err := refClient.ListServices()
+	if err != nil {
+		panic(errors.Wrap(err, "list services"))
+	}
+	var allowedRequestHeaders = sidecarConfig.AllowRequestHeaders
+	for _, serviceName := range serviceNames {
+		fmt.Println(serviceName)
+		if strings.HasPrefix(serviceName, "grpc.reflection") {
+			fmt.Println("skip service:", serviceName)
+			continue
+		}
+		// 从reflection获取服务描述符
+		svcDesc, err := refClient.ResolveService(serviceName)
+		if err != nil {
+			panic(errors.Wrapf(err, "resolve service:%s", serviceName))
+		}
+		doResolveService(engine,
+			serviceName, svcDesc, conn, allowedRequestHeaders)
+	}
+}
+
+func doResolveService(engine gin.IRouter,
+	serviceName string, svcDesc *desc.ServiceDescriptor, serviceClientConn *grpc.ClientConn,
+	allowedRequestHeaders []string) {
+	var methods = svcDesc.GetMethods()
+	var reflector = openapi3.NewReflector()
+	var specDesc = svcDesc.GetName()
+	reflector.Spec = &openapi3.Spec{
+		Openapi: "3.0.3",
+		Info: openapi3.Info{
+			Title:       svcDesc.GetName(),
+			Description: &specDesc,
+			Version:     "1.0.0",
+		},
+	}
+	for _, method := range methods {
+		var methodName = method.GetName()
+		fmt.Println(methodName)
+		if method.IsServerStreaming() || method.IsClientStreaming() {
+			fmt.Println("skip method:", methodName, "streaming not supported")
+			continue
+		}
+		var options = method.GetOptions()
+		fmt.Println(options)
+		//httpRule := &annotations.HttpRule{}
+		httpRuleObj, err := proto.GetExtension(options, annotations.E_Http)
+		if err != nil {
+			var internalErr = errors.Wrap(err, "get http rule")
+			fmt.Println("skip method:", methodName, "err:", internalErr)
+			continue
+		}
+		httpRule, ok := httpRuleObj.(*annotations.HttpRule)
+		if !ok {
+			panic(errors.Wrap(err, "cast to HttpRule"))
+		}
+		httpMethod, httpPath := http_rule.HttpRulePatternToMethod(httpRule)
+		fmt.Println(fmt.Sprintf("%s->%s", serviceName, methodName), httpMethod, httpPath)
+		//swagger op
+		if err := addOpenAPIOperation(httpMethod, httpPath,
+			reflector,
+			method); err != nil {
+			panic(errors.Wrap(err, "add openapi operation"))
+		}
+		//service handler
+		engine.Handle(httpMethod, httpPath,
+			newServiceHandler(serviceClientConn, method, allowedRequestHeaders))
+	}
+	apiDocData, err := reflector.Spec.MarshalYAML()
+	if err != nil {
+		panic(errors.Wrap(err, "marshal openapi spec to yaml"))
+	}
+	//var apiDocStr = string(apiDocData)
+	var openapiHttpPath = fmt.Sprintf("/openapi/%s.yaml", svcDesc.GetName())
+	engine.GET(openapiHttpPath, func(c *gin.Context) {
+		c.Data(http.StatusOK, "text/yaml", apiDocData)
+	})
+	var swaggerUIHttpPath = fmt.Sprintf("/swagger-ui/%s.html", svcDesc.GetName())
+	engine.GET(swaggerUIHttpPath, func(c *gin.Context) {
+		var encodedApiDocStr = url.PathEscape(string(apiDocData))
+		var dataUrl = fmt.Sprintf("data:text/plain,%s", encodedApiDocStr)
+		htmlStr, err := static.GenerateFullHtml(dataUrl)
+		if err != nil {
+			var internalErr = errors.Wrap(err, "generate full html")
+			c.String(http.StatusInternalServerError, internalErr.Error())
+			return
+		}
+		c.Data(http.StatusOK, "text/html", []byte(htmlStr))
+	})
+}
+
+func addOpenAPIOperation(httpMethod string, httpPath string,
+	reflector *openapi3.Reflector,
+	method *desc.MethodDescriptor) error {
+	const contentType = "application/json"
+	opCtx, err := reflector.NewOperationContext(httpMethod, httpPath)
+	if err != nil {
+		return errors.Wrap(err, "new openapi operation")
+	}
+	//apply api document
+	var options = method.GetOptions()
+	operationExtObj, err := proto.GetExtension(options, openAPIV3Proto.E_Operation)
+	if err != nil {
+		var internalErr = errors.Wrap(err, "get operation proto extension")
+		fmt.Println("[openapi] skip generate operation:", method.GetFullyQualifiedName(), "err:", internalErr)
+	} else {
+		var operationExt = operationExtObj.(*openAPIV3Proto.Operation)
+		opCtx.SetDescription(operationExt.Description)
+	}
+	var convertCtx = &protoMessageToGoStructContext{
+		generatedMap: make(map[string]reflect.Type),
+	}
+	reqTemplateType, err := protoMessageToGoStruct(method.GetInputType(), convertCtx)
+	if err != nil {
+		return errors.Wrap(err, "convert grpc request msg")
+	}
+	var reqTemplate = reflect.New(reqTemplateType).Elem().Interface()
+	opCtx.AddReqStructure(reqTemplate,
+		openapi.WithContentType(contentType))
+	respTemplateType, err := protoMessageToGoStruct(method.GetOutputType(), convertCtx)
+	if err != nil {
+		return errors.Wrap(err, "convert grpc response msg")
+	}
+	var respTemplate = reflect.New(respTemplateType).Elem().Interface()
+	opCtx.AddRespStructure(respTemplate,
+		openapi.WithContentType(contentType))
+	if err := reflector.AddOperation(opCtx); err != nil {
+		return errors.Wrap(err, "add operation to openapi reflector")
+	}
+	return nil
+}
+
+type protoMessageToGoStructContext struct {
+	generatedMap map[string]reflect.Type
+}
+
+func protoMessageToGoStruct(messageDesc *desc.MessageDescriptor, convertCtx *protoMessageToGoStructContext) (reflect.Type, error) {
+	var structFields = make([]reflect.StructField, 0)
+	var protoFields = messageDesc.GetFields()
+	var dynamicMsg = dynamic.NewMessage(messageDesc)
+	//var selfCycleFieldNameList = make([]string, 0)
+	for _, protoField := range protoFields {
+		var protoFieldName = protoField.GetName()
+		var dynamicFieldValue = dynamicMsg.GetField(protoField)
+		var fieldProtoType = protoField.GetType()
+		var fieldReflectType = reflect.Type(nil)
+		switch fieldProtoType {
+		case descriptorpb.FieldDescriptorProto_TYPE_MESSAGE:
+			var fieldProtoMsgType = protoField.GetMessageType()
+			if fieldProtoMsgType.GetFullyQualifiedName() == messageDesc.GetFullyQualifiedName() {
+				fmt.Println("self cycle detected:", fieldProtoMsgType.GetFullyQualifiedName())
+				//selfCycleFieldNameList = append(selfCycleFieldNameList, protoFieldName)
+				fieldReflectType = reflect.TypeOf("self cycle field")
+				break
+			}
+			msgType, err := protoMessageToGoStruct(fieldProtoMsgType, convertCtx)
+			if err != nil {
+				return nil, errors.Wrapf(err, "convert message type for field:%s.%s",
+					messageDesc.GetName(), protoFieldName)
+			}
+			fieldReflectType = msgType
+			break
+
+		//case descriptorpb.FieldDescriptorProto_TYPE_ENUM:
+		//	fieldReflectType=reflect.TypeOf(int(0))
+		//	break
+		default:
+			fieldReflectType = reflect.TypeOf(dynamicFieldValue)
+		}
+		var fieldTagStr = ""
+		//先检查有没有openapi option
+		var fieldOptions = protoField.GetOptions()
+		propertyExtObj, err := proto.GetExtension(fieldOptions, openAPIV3Proto.E_Property)
+		if err != nil {
+			var internalErr = errors.Wrap(err, "get schema proto extension")
+			fmt.Println("[openapi] skip generate schema:", protoField.GetFullyQualifiedName(), "err:", internalErr)
+		} else {
+			var schemaExt = propertyExtObj.(*openAPIV3Proto.Schema)
+			fieldTagStr = fmt.Sprintf(`%s title:"%s" description:"%s"`,
+				fieldTagStr, schemaExt.Title, schemaExt.Description)
+		}
+		//处理repeated和optional这种label
+		var fieldProtoLabel = protoField.GetLabel()
+		switch fieldProtoLabel {
+		case descriptorpb.FieldDescriptorProto_LABEL_REPEATED:
+			fieldReflectType = reflect.SliceOf(fieldReflectType)
+		}
+		fieldTagStr = fmt.Sprintf(`%s json:"%s"`, fieldTagStr, protoField.GetName())
+		var reflectFieldName = strings.ToUpper(string(protoFieldName[0])) + protoFieldName[1:]
+		var structField = reflect.StructField{
+			Name: reflectFieldName,
+			Type: fieldReflectType,
+			Tag:  reflect.StructTag(fieldTagStr),
+		}
+		structFields = append(structFields, structField)
+	}
+	var reflectMsgType = reflect.StructOf(structFields)
+	return reflectMsgType, nil
+}
+
+func unicodeEncode(str string) string {
+	textQuoted := strconv.QuoteToASCII(str)
+	textUnquoted := textQuoted[1 : len(textQuoted)-1]
+	return textUnquoted
+}
